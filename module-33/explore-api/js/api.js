4e6890fc801a56a24222bc3082376d696308fd6d
mahamudul hasan seber <sebermh@gmail.com>
2024-10-01 01:28:14 +0600

    module-33

---
+++ module-33/explore-api/js/api.js
@@ -0,0 +1,2 @@
+const user = { id: 1, name: 'ssssss', job: 'actor' };
+console.log(user);
---
+++ module-33/explore-api/js/fetch.js
@@ -0,0 +1,18 @@
+// console.log('fetching')
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then(response => console.log(response))
+// .then(json => console.log(json))
+
+
+// fetch(url)
+// .then(response => response.json())
+// .then(json=>console.log(json))
+
+function loadData() {
+    const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+    fetch(url)
+        .then(response => response.json())
+        .then(json => console.log(json))
+}
